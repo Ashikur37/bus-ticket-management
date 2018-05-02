851c5c11fd3ec423d9284ba8988868d34419e1bb
Ashikur37 <piash3700@gmail.com>
2018-05-02 18:27:26 +0600

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
+</html>
